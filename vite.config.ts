5195416e1fa5fdd910569e8aa9e56dad8c8892ea
7nohe <7nohe.urata@gmail.com>
2022-10-07 23:31:38 +0900

    feat: optimize build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import packageJson from "./package.json";
 
 export default defineConfig({
   resolve: {
@@ -13,6 +14,21 @@ export default defineConfig({
       entry: resolve(__dirname, "src/index.ts"),
       name: "ReactRealtimeCursor",
       fileName: "react-realtime-cursor",
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external:
+        [
+          ...Object.keys(packageJson.devDependencies),
+          "firebase/database",
+          "firebase/auth",
+        ] || [],
+      output: {
+        globals: {
+          react: "React",
+          "aws-amplify": "awsAmplify",
+        },
+      },
     },
   },
   plugins: [dts()],
